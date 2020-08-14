24b09b57a2910321850816deb526c5c2d2807106
Xiaohui Lu <xlu3@bmr013.srl.ford.com>
2020-08-14 15:37:24 -0400

    Added rank/select in BST

--- src/main/BST.java
+++ src/main/BST.java
@@ -38,38 +38,63 @@ public class BST<Key extends Comparable<Key>, Value>
         int cmp = key.compareTo(x.key);
         if      ( cmp < 0 ) x.left  = put(x.left,  key, val);
         else if ( cmp > 0 ) x.right = put(x.right, key, val);
-        else x.val = val;
+        else                x.val = val;
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
 
     public Value get(Key key)
     {
-        Node x = get(root, key);
-        if ( x == null ) return null;
-        return x.val;
+        return get(root, key);
     }
 
-    public Node get(Node x, Key key)
+    public Value get(Node x, Key key)
     {
         if ( x == null ) return null;
         int cmp = key.compareTo(x.key);
-        if (cmp < 0)      return get(x.left,  key);
+        if      (cmp < 0) return get(x.left,  key);
         else if (cmp > 0) return get(x.right, key);
-        else return x;
+        else              return x.val;
+    }
+
+    public int rank(Key key)
+    {   return rank(root, key); }
+
+    public int rank(Node x, Key key)
+    {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return rank(x.left, key);
+        else if (cmp > 0) return size(x.left) + 1 + rank(x.right, key);
+        else              return size(x.left);
+    }
+
+    public Key select(int n)
+    {   return select(root, n).key; }
+
+    public Node select(Node x, int k)
+    {
+        if (x == null) return null;
+        int t = size(x.left);
+        if      (t > k) return select(x.left,  k);
+        else if (t < k) return select(x.right, k-t-1);
+        else            return x;
     }
 
     public static void main(String[] args)
     {
         BST<Character, Integer> bst = new BST<Character, Integer>();
+
         bst.put('E', 5);
         bst.put('X', 24);
         bst.put('A', 1);
         bst.put('M', 13);
         bst.put('P', 14);
         bst.put('L', 12);
-        bst.put('E', 5);
-        System.out.println(bst.size());
-        System.out.println(bst.root.left.key);
+        bst.put('E', 0);
+        //System.out.println(bst.size());
+        //System.out.println(bst.root.left.key);
+        System.out.println(bst.rank('A'));
+        System.out.println(bst.select(3));
     }
 }
